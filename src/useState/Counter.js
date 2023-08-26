5cd23b7a2e14d821a306187e2298d269c1742810
Sani_flex <80122168+Saniflex-1@users.noreply.github.com>
2023-08-26 03:34:20 +0100

    Update Counter.js

--- src/useState/Counter.js
+++ src/useState/Counter.js
@@ -5,7 +5,7 @@ const Counter = () => {
     const [value , setValue] = useState(0);
     const increment =()=>{
         setValue(value + 1)
-        
+       
     }
 
     const moreComplex = ()=>{
